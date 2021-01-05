827bde53e41e8f981ec76bb8c3c63d59af27af99
Crowdbotics <team@crowdbotics.com>
2021-01-05 01:26:37 +0000

    Update models

--- my_crypto_portfolio_23673/wsgi.py
+++ my_crypto_portfolio_23673/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_crypto_portfolio_23673.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_crypto_portfolio_23673.settings")
 
 application = get_wsgi_application()
